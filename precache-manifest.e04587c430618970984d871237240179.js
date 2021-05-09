042310d971ea10455d202c4098c64367c27e94c3
Gia Bao Pham <giabao_pham1982vn@yahoo.com>
2021-05-09 11:47:06 +0700

    'Initialize'

---
+++ precache-manifest.e04587c430618970984d871237240179.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d3e0c44ad30ddefb922969f69b20c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc833a65364dd6588a1a",
+    "url": "/static/css/2.4fdeb0ab.chunk.css"
+  },
+  {
+    "revision": "ef41f41fba0ba7e97d93",
+    "url": "/static/css/main.e5154bea.chunk.css"
+  },
+  {
+    "revision": "dc833a65364dd6588a1a",
+    "url": "/static/js/2.07279c53.chunk.js"
+  },
+  {
+    "revision": "713acd2dac6c7d6e42a0e19bc8c5c98d",
+    "url": "/static/js/2.07279c53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef41f41fba0ba7e97d93",
+    "url": "/static/js/main.49552d0c.chunk.js"
+  },
+  {
+    "revision": "f1895eaa381970f56705",
+    "url": "/static/js/runtime-main.6f177c5c.js"
+  }
+]);
